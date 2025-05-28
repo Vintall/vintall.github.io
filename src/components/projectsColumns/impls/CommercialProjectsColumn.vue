6348ce6278165d07f67d9e59ff5d2b653b2a88b5
Egor Ilyashenko <eg.ilyashenko@gmail.com>
2025-05-28 23:33:24 +0300

    ElementalWars preview fix

--- src/components/projectsColumns/impls/CommercialProjectsColumn.vue
+++ src/components/projectsColumns/impls/CommercialProjectsColumn.vue
@@ -2,16 +2,10 @@
 import ProjectsColumn from '../ProjectsColumn.vue';
 import GameCard from '@/components/GameCard.vue'
 import MergeGamesData from '@/components/projects/Commercial/MergeGames/MergeGamesData.js'
+import ElementalWarsPreview from '@/assets/media/projects/elementalWars.png'
 import { ref } from 'vue';
 
 const mergeGamesTags = MergeGamesData()
-const elementalWarsTags = ref([
-    { 
-        name: "Elemental Wars", 
-        img: "https://play-lh.googleusercontent.com/vwzBQXN7Ghb8XrbxBNEHI_hFRCfVdqVm-DRFWlYeFWivQAy5RbOJ4yHeWllmFRC5vBK9=w240-h480",
-        link: "https://play.google.com/store/apps/details?id=elemental.battler.nft.card.meowgon&hl=en",
-    }
-])
 </script>
 
 <template>
@@ -23,7 +17,7 @@ const elementalWarsTags = ref([
         </GameCard>
         <GameCard 
             name="Elemental Wars"
-            img="https://play-lh.googleusercontent.com/OGXURSY54hmZKrfDb8MDUkCeSbwwvmgFJBwJXYdSEsVrLsZiLUO26PAN0l1UpAHB80o=w526-h296" 
+            :img=ElementalWarsPreview 
             link="https://play.google.com/store/apps/details?id=elemental.battler.nft.card.meowgon&hl=en">
             Turn-based deck-building strategy game. PvP&PvE adventures. Characters consists of 3 body parts. 
             Assemble your own combination to adjust for you play-style. <br> <span class="text-amber-200">Target Platform: Google Play</span>
