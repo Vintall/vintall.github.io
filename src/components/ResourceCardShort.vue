028710b37270d43ef7858d275376156491be5f9c
Egor Ilyashenko <eg.ilyashenko@gmail.com>
2025-05-28 12:05:36 +0300

    Added slow-ugly switch and short version of ResourceCard

---
+++ src/components/ResourceCardShort.vue
@@ -0,0 +1,36 @@
+<script setup>
+import ResourceCardTag from './ResourceCardTag.vue';
+import { getTagById } from '@/assets/data/ResourcesData';
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: false,
+  },
+  tagIds: {
+    type: NodeList,
+    required: false
+  }
+})
+</script>
+
+<template>
+  <a class="flex flex-col flex-wrap text-xl text-gray-200 text-balance bg-gray-800 hover:bg-gray-900 rounded-2xl m-2 p-2 gap-2 border-gray-300 border-3 shadow-2xl shadow-gray-900" :href="link">
+    <p class="w-full text-wrap">
+      <span class="text-2xl font-extrabold">{{ name }} - </span> 
+      {{ desc }}
+    </p>
+    <div class="flex flex-row flex-wrap w-full gap-1">
+      <div v-for="tagId in tagIds">
+        <ResourceCardTag :color="getTagById(tagId).color" :text="getTagById(tagId).text"></ResourceCardTag>
+      </div>
+    </div>
+  </a>
+</template>
--- src/components/pages/ResourcesView.vue
+++ src/components/pages/ResourcesView.vue
@@ -3,6 +3,7 @@ import { useRoute, useRouter } from 'vue-router'
 import { ref, computed, watch } from 'vue'
 import ContentView from '@/components/core/ContentView.vue';
 import ResourceCard from '@/components/ResourceCard.vue';
+import ResourceCardShort from '@/components/ResourceCardShort.vue';
 import { resourceCardsList, getTagById, tags } from '@/assets/data/ResourcesData.js';
 import ResourceCardTag from '../ResourceCardTag.vue';
 
@@ -50,33 +51,53 @@ const removeTag = (_id) => {
     selectedTagIds.value = selectedTagIds.value.filter(id => id !== _id)
 }
 
+const isOn = ref(false)
 </script>
 
 <template>
     <div>
         <ContentView>
-            <div class="flex flex-col text-xl text-balance bg-gray-800 rounded-2xl m-2 pt-3 px-3 border-gray-500 border-3 shadow-2xl shadow-gray-900">
-                <h1 class="text-gray-300 text-xl mb-1">Filters</h1>
-                <div class="mb-4 flex flex-wrap gap-2 min-h-8">
-                    <label v-for="tag in tags" :key="tag.id" class="flex items-center space-x-2">
-                    <button type="button" @click="addTag(tag.id)">
-                        <ResourceCardTag class="hover:outline-1 hover:outline-amber-200" :color="tag.color" :text="tag.text"></ResourceCardTag>
-                    </button>
-                    </label>
-                </div>
-                <div class="flex w-full h-0.5 bg-gray-500"></div>
-                <h1 class="text-gray-300 text-xl mb-1">Selected Filters</h1>
-                <div class="mb-4 flex flex-wrap gap-2 min-h-8">
-                    <label v-for="tagId in selectedTagIds" :key="tagId" class="flex items-center space-x-2">
-                    <button type="button" @click="removeTag(tagId)">
-                        <ResourceCardTag class="hover:outline-1 hover:outline-amber-200" :color="getTagById(tagId).color" :text="getTagById(tagId).text"></ResourceCardTag>
-                    </button>
-                    </label>
-                </div>
+            <div class="flex flex-col text-balance bg-gray-800 rounded-2xl m-2 pt-3 px-3 border-gray-500 border-3 shadow-2xl shadow-gray-900 text-gray-300 text-xl mb-1">
+              
+              <div class="flex flex-row">
+                <span class="h-9 mr-2">List Mode: </span>
+                <button 
+                  @click="isOn = !isOn" 
+                  :class="['relative w-26 h-8 rounded-md transition-colors duration-100 bg-gray-700']" 
+                  class="hover:outline-1 hover:outline-cyan-600">
+                    <span 
+                      :class="['absolute left-0 top-0 w-13 h-8 bg-gray-500 rounded-md shadow transform transition-transform duration-100',
+                      isOn ? 'translate-x-13' : 'translate-x-0']">
+                    </span>
+                    <span class="absolute left-1 bottom-1 transition-transform duration-100" :class="[isOn ? 'text-gray-500' : 'text-cyan-300']">Slow</span>
+                    <span class="absolute right-1 bottom-1 transition-transform duration-100" :class="[isOn ? 'text-cyan-300' : 'text-gray-500']">Ugly</span>
+                </button>
+              </div>
+              
+              <h1>Filters</h1>
+              <div class="mb-4 flex flex-wrap gap-2 min-h-8">
+                  <label v-for="tag in tags" :key="tag.id" class="flex items-center space-x-2">
+                  <button type="button" @click="addTag(tag.id)">
+                      <ResourceCardTag class="hover:outline-1 hover:outline-cyan-600" :color="tag.color" :text="tag.text"></ResourceCardTag>
+                  </button>
+                  </label>
+              </div>
+              <div class="flex w-full h-0.5 bg-gray-500"></div>
+              <h1>Selected Filters</h1>
+              <div class="mb-4 flex flex-wrap gap-2 min-h-8">
+                  <label v-for="tagId in selectedTagIds" :key="tagId" class="flex items-center space-x-2">
+                  <button type="button" @click="removeTag(tagId)">
+                      <ResourceCardTag class="hover:outline-1 hover:outline-cyan-600" :color="getTagById(tagId).color" :text="getTagById(tagId).text"></ResourceCardTag>
+                  </button>
+                  </label>
+              </div>
             </div>
             
-            <div v-for="resourceCard in filteredItems">
+            <div v-if="!isOn" v-for="resourceCard in filteredItems">
                 <ResourceCard :name="resourceCard.name" :link="resourceCard.link" :img="resourceCard.img" :desc="resourceCard.desc" :tagIds="resourceCard.tagIds"></ResourceCard>
+            </div>
+            <div v-if="isOn" v-for="resourceCard in filteredItems">
+                <ResourceCardShort :name="resourceCard.name" :link="resourceCard.link" :img="resourceCard.img" :desc="resourceCard.desc" :tagIds="resourceCard.tagIds"></ResourceCardShort>
             </div>
         </ContentView>
     </div>
