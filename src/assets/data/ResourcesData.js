b86f01334eae54dab2285a7571d952fd35fed0df
Egor Ilyashenko <eg.ilyashenko@gmail.com>
2025-06-02 11:09:31 +0300

    New Resources

--- src/assets/data/ResourcesData.js
+++ src/assets/data/ResourcesData.js
@@ -7,6 +7,13 @@ import FreepikPreview from "@/assets/media/resources/freepik.png"
 import PalettonPreview from "@/assets/media/resources/paletton.png"
 import CaesiumImageCreatorPreview from "@/assets/media/resources/caesiumImageCreator.png"
 import AlternativeToPreview from "@/assets/media/resources/alternativeTo.png"
+import ShadertoyPreview from '@/assets/media/resources/shadertoy.png'
+import SensortowerPreview from '@/assets/media/resources/sensortower.png'
+import FreeSoundPreview from '@/assets/media/resources/freesound.png'
+import FreeSfxPreview from '@/assets/media/resources/freesfx.png'
+import CGALPreview from '@/assets/media/resources/cgal.png'
+import SteamDBPreview from '@/assets/media/resources/steamDB.png'
+import PixabayPreview from '@/assets/media/resources/pixabay.png'
 //Tags: HLSL, ShaderGraph, Math
 
 export const getTagById = (id) => tags.value.find(tag => tag.id === id)
@@ -18,16 +25,17 @@ export const tags = ref([
   { id: 'HLSL', text: 'HLSL', color: 'bg-lime-500/50' },
   { id: 'CG', text: 'CG', color: 'bg-green-500/50' },
   { id: 'VFX', text: 'VFX', color: 'bg-emerald-500/50' },
+  { id: 'SFX', text: 'SFX', color: 'bg-emerald-500/50' },
   { id: 'Solid', text: 'SOLID', color: 'bg-cyan-500/50' },
   { id: 'FreeAssets', text: 'Assets', color: 'bg-sky-500/50' },
   { id: 'Design', text: 'Design', color: 'bg-blue-500/50' },
   { id: 'Market', text: 'Market Research', color: 'bg-indigo-500/50' },
-  { id: 'Steam', text: 'Steam', color: 'bg-violet-500/50' },
   { id: 'Repo', text: 'Repository', color: 'bg-fuchsia-500/50' },
   { id: 'Program', text: 'Program', color: 'bg-pink-500/50' },
   { id: 'WebResource', text: 'Web Resource', color: 'bg-rose-500/50' },
   { id: 'Tools', text: 'Tools', color: 'bg-stone-500/50' },
   { id: 'AI', text: 'AI', color: 'bg-stone-500/50' },
+  { id: 'Networking', text: 'Networking', color: 'bg-lime-300/50'}
 ])
 
 const resourceCards = ref([
@@ -90,9 +98,51 @@ const resourceCards = ref([
     {
         name: "Sensor Tower",
         link: "https://sensortower.com/",
-        img: "",
-        desc: "",
+        img: SensortowerPreview,
+        desc: "Measure the world’s digital economy with Sensor Tower",
+        tagIds: ['Market']
+    },
+    {
+        name: "SteamDB",
+        link: "https://steamdb.info/",
+        img: SteamDBPreview,
+        desc: "This third-party website gives you better insight into the Steam platform and everything in its database.",
         tagIds: ['Gamedev', 'Market']
+    },
+    {
+        name: "Shadertoy",
+        link: "https://www.shadertoy.com/",
+        img: ShadertoyPreview,
+        desc: "Build and Share your best shaders with the world and get Inspired",
+        tagIds: ['Shaders']
+    },
+    {
+        name: "The Computational Geometry Algorithms Library",
+        link: "https://www.cgal.org/",
+        img: CGALPreview,
+        desc: "CGAL is an open source software project that provides easy access to efficient and reliable geometric algorithms in the form of a C++ library.",
+        tagIds: []
+    },
+    {
+        name: "FreeSFX",
+        link: "https://freesfx.co.uk/",
+        img: FreeSfxPreview,
+        desc: "Browse selection of Free Sound Effects and Music",
+        tagIds: ['FreeAssets', 'SFX']
+    },
+    {
+        name: "Freesound",
+        link: "https://freesound.org/",
+        img: FreeSoundPreview,
+        desc: "Find any sound you like",
+        tagIds: ['FreeAssets', 'SFX']
+    },
+    {
+        name: "Pixabay",
+        link: "https://pixabay.com/",
+        img: PixabayPreview,
+        desc: "Stunning royalty-free images & royalty-free stock",
+        tagIds: ['FreeAssets', 'SFX']
     }
    /*,
     {
